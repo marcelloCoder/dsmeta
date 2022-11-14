71cf8c7f3d5395ed33353ca34e4d38e9cdac09c3
Marcello Vieira Mueno <deoliveiral767@gmail.com>
2022-11-14 18:57:54 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
-import NotificationButton from "./assets/components/NotificationButton"
+import NotificationButton from "./assets/components/NotificationButton";
+import Header from "./assets/components/Header";
+import SalesCard from "./assets/components/SalesCard";
 
 function App() {
-  return(
+  return (
     <>
-      <h1>Hello World</h1>
-      <NotificationButton />
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
