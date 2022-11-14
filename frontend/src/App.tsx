124a15080e615c3083378556fdb6e1bc8338fa24
Marcello Vieira Mueno <deoliveiral767@gmail.com>
2022-11-14 18:32:56 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotificationButton from "./assets/components/NotificationButton"
 
 function App() {
   return(
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
